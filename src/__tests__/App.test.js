4105e9795c3bd805b1cc79a07b9314cbdeb3db33
UliKuch <u.kuchelmeister@gmx.de>
2019-11-12 12:11:04 +0100

    Add mock redux store for testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from '../App';
 
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "../store/reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+import configureStore from "redux-mock-store";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+// Initialize mock store with state
+const initialState = {
+  city: {
+    cities: []
+  },
+  user: {
+    userEmail: "",
+    userImage: "",
+    userId: ""
+  }
+};
+const store = mockStore(initialState);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
